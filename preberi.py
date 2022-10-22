e317cf74dc8749eff66b269522e3b60d7eaeb071
TerezijaKrecic <krecic.terezija@gmail.com>
2022-10-22 17:00:23 +0200

    prebere 5 podatkov vsake gore jul. alp + csv, json

--- preberi.py
+++ preberi.py
@@ -21,6 +21,9 @@ datoteka_julijske_alpe = 'A_julijske_alpe.html'
 datoteka_karavanke = 'A_karavanke.html'
 datoteka_kamn_sav_alpe = 'A_kamn_sav_alpe.html'
 
+csv_julijske_alpe = 'A_julijske_alpe.csv'
+json_julijske_alpe = 'A_julijske_alpe.json'
+
 # Shranimo glavne spletne strani o gorah v svoje mape:
 orodja.shrani_spletno_stran(url_julijske_alpe, datoteka_julijske_alpe, mapa_julijske_alpe)
 orodja.shrani_spletno_stran(url_karavanke, datoteka_karavanke, mapa_karavanke)
@@ -36,7 +39,15 @@ vzorec_ime_in_url = re.compile(
     r'>(?P<ime>.+?)<.*?\1"',
     re.DOTALL
 )
-
+vzorec_podatki = re.compile(
+    r'<h1>(?P<ime>.*?)<\/h1>.'
+    r'*?Država:<\/b>.*?>'
+    r'(?P<drzava>.*?)<.*?'
+    r'Gorovje:<\/b>.*?>(?P<gorovje>.*?)<.*?'
+    r'Višina:\D*(?P<visina>\d*).*?',
+    re.DOTALL
+)
+vzorec_koordinate = re.compile(r'Širina.*?span.*?>(?P<koordinate>.*?)<', re.DOTALL)
 
 ###################
 # julijske alpe
@@ -49,6 +60,23 @@ julijske_alpe_vsebina = orodja.vsebina_datoteke(os.path.join(mapa_julijske_alpe,
 seznam_html_podatkov = re.findall(vzorec_blok_gora, julijske_alpe_vsebina)
 
 # iz posameznega odseka preberemo url s podrobnostmi in ta html shranimo v isto mapo pod imenom 'ime_gore'.html
+seznam_podatkov = []
 for blok in seznam_html_podatkov:
     slovar_ime_url = vzorec_ime_in_url.search(blok).groupdict()
-    orodja.shrani_spletno_stran(url_hribi + slovar_ime_url['url'], slovar_ime_url['ime'].replace(' ','_').replace('/','_') + '.html', mapa_julijske_alpe)
+    url = url_hribi + slovar_ime_url['url']
+    ime = slovar_ime_url['ime'].replace(' ','_').replace('/','_') + '.html'
+    orodja.shrani_spletno_stran(url, ime, mapa_julijske_alpe)
+    # zberimo koristne podatke:
+    html_vsebina = orodja.vsebina_datoteke(os.path.join(mapa_julijske_alpe, ime))
+    slovar = vzorec_podatki.search(html_vsebina).groupdict()
+    slovar['visina'] = int(slovar['visina'])
+    koordinate = re.search(vzorec_koordinate, html_vsebina)
+    if koordinate is None:
+        slovar['koordinate'] = 'Brez podatka'
+    else:
+        slovar['koordinate'] = koordinate.group('koordinate').replace('&nbsp;',' ')
+    seznam_podatkov.append(slovar)
+
+# zapišimo to v csv in json datoteko:
+orodja.zapisi_csv(seznam_podatkov, seznam_podatkov[0].keys(), os.path.join(mapa_julijske_alpe, csv_julijske_alpe))
+orodja.zapisi_json(seznam_podatkov, os.path.join(mapa_julijske_alpe, json_julijske_alpe))

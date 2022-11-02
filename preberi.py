d3302c786c46a98d73eeb86ac5e0df4a9512cc55
TerezijaKrecic <krecic.terezija@gmail.com>
2022-11-02 08:42:38 +0100

    popravljen program - izpis podatkov vseh 3 gorovij

--- preberi.py
+++ preberi.py
@@ -7,27 +7,19 @@ import os
 url_hribi = 'https://www.hribi.net'
 
 # url glavnih treh strani:
-url_julijske_alpe = 'https://www.hribi.net/gorovje/julijske_alpe/1'
-url_karavanke = 'https://www.hribi.net/gorovje/karavanke/11'
-url_kamn_sav_alpe = 'https://www.hribi.net/gorovje/kamnisko_savinjske_alpe/3'
+url_gorovij = ['https://www.hribi.net/gorovje/julijske_alpe/1', 'https://www.hribi.net/gorovje/karavanke/11', 'https://www.hribi.net/gorovje/kamnisko_savinjske_alpe/3']
 
 # pot do mape, kjer shranimo posamezne podatke
-mapa_julijske_alpe = 'podatki\\julijske_alpe'
-mapa_karavanke = 'podatki\\karavanke'
-mapa_kamn_sav_alpe = 'podatki\\kamn_sav_alpe'
+mapa_gorovij = ['podatki\\julijske_alpe', 'podatki\\karavanke', 'podatki\\kamn_sav_alpe']
 
 # ime html datotek z vsebino glavnih treh strani (spredaj je AA, da se pojavi na vrhu strani)
-datoteka_julijske_alpe = 'A_julijske_alpe.html'
-datoteka_karavanke = 'A_karavanke.html'
-datoteka_kamn_sav_alpe = 'A_kamn_sav_alpe.html'
+datoteka_gorovij = ['A_julijske_alpe.html', 'A_karavanke.html', 'A_kamn_sav_alpe.html']
 
-csv_julijske_alpe = 'A_julijske_alpe.csv'
-json_julijske_alpe = 'A_julijske_alpe.json'
+# ime csv datotek z izluščenimi podatki vsake od treh gravnih strani (spredaj je AA, da se pojavi na vrhu strani)
+csv_gorovij = ['A_julijske_alpe.csv', 'A_karavanke.csv', 'A_kamn_sav_alpe.csv']
 
-# Shranimo glavne spletne strani o gorah v svoje mape:
-orodja.shrani_spletno_stran(url_julijske_alpe, datoteka_julijske_alpe, mapa_julijske_alpe)
-orodja.shrani_spletno_stran(url_karavanke, datoteka_karavanke, mapa_karavanke)
-orodja.shrani_spletno_stran(url_kamn_sav_alpe, datoteka_kamn_sav_alpe, mapa_kamn_sav_alpe)
+# ime json datotek z izluščenimi podatki vsake od treh gravnih strani (spredaj je AA, da se pojavi na vrhu strani)
+json_gorovij = ['A_julijske_alpe.json', 'A_karavanke.json', 'A_kamn_sav_alpe.json']
 
 # vzorci za iskanje:
 vzorec_blok_gora = re.compile(
@@ -50,33 +42,37 @@ vzorec_podatki = re.compile(
 vzorec_koordinate = re.compile(r'Širina.*?span.*?>(?P<koordinate>.*?)<', re.DOTALL)
 
 ###################
-# julijske alpe
+# ZBEREMO PODATKE
 ###################
 
-# Niz s html vsebino glavne strani julijskih alp:
-julijske_alpe_vsebina = orodja.vsebina_datoteke(os.path.join(mapa_julijske_alpe, datoteka_julijske_alpe))
+for i in range(len(url_gorovij)):
+    # Shranimo glavne spletne strani o gorah v svoje mape:
+    orodja.shrani_spletno_stran(url_gorovij[i], datoteka_gorovij[i], mapa_gorovij[i])
 
-# izluščimo odsek html-ja za posamezno goro:
-seznam_html_podatkov = re.findall(vzorec_blok_gora, julijske_alpe_vsebina)
+    # Niz s html vsebino glavne strani posameznega gorovja:
+    gorovje_vsebina = orodja.vsebina_datoteke(os.path.join(mapa_gorovij[i], datoteka_gorovij[i]))
 
-# iz posameznega odseka preberemo url s podrobnostmi in ta html shranimo v isto mapo pod imenom 'ime_gore'.html
-seznam_podatkov = []
-for blok in seznam_html_podatkov:
-    slovar_ime_url = vzorec_ime_in_url.search(blok).groupdict()
-    url = url_hribi + slovar_ime_url['url']
-    ime = slovar_ime_url['ime'].replace(' ','_').replace('/','_') + '.html'
-    orodja.shrani_spletno_stran(url, ime, mapa_julijske_alpe)
-    # zberimo koristne podatke:
-    html_vsebina = orodja.vsebina_datoteke(os.path.join(mapa_julijske_alpe, ime))
-    slovar = vzorec_podatki.search(html_vsebina).groupdict()
-    slovar['višina'] = int(slovar['višina'])
-    koordinate = re.search(vzorec_koordinate, html_vsebina)
-    if koordinate is None:
-        slovar['koordinate'] = 'Brez podatka'
-    else:
-        slovar['koordinate'] = koordinate.group('koordinate').replace('&nbsp;',' ')
-    seznam_podatkov.append(slovar)
+    # izluščimo odsek html-ja za posamezno goro:
+    seznam_html_podatkov = re.findall(vzorec_blok_gora, gorovje_vsebina)
 
-# zapišimo to v csv in json datoteko:
-orodja.zapisi_csv(seznam_podatkov, seznam_podatkov[0].keys(), os.path.join(mapa_julijske_alpe, csv_julijske_alpe))
-orodja.zapisi_json(seznam_podatkov, os.path.join(mapa_julijske_alpe, json_julijske_alpe))
+    # iz posameznega odseka preberemo url s podrobnostmi in ta html shranimo v isto mapo pod imenom 'ime_gore'.html
+    seznam_podatkov = []
+    for blok in seznam_html_podatkov:
+        slovar_ime_url = vzorec_ime_in_url.search(blok).groupdict()
+        url = url_hribi + slovar_ime_url['url']
+        ime = slovar_ime_url['ime'].replace(' ','_').replace('/','_') + '.html'
+        orodja.shrani_spletno_stran(url, ime, mapa_gorovij[i])
+        # zberimo koristne podatke:
+        html_vsebina = orodja.vsebina_datoteke(os.path.join(mapa_gorovij[i], ime))
+        slovar = vzorec_podatki.search(html_vsebina).groupdict()
+        slovar['višina'] = int(slovar['višina'])
+        koordinate = re.search(vzorec_koordinate, html_vsebina)
+        if koordinate is None:
+            slovar['koordinate'] = 'Brez podatka'
+        else:
+            slovar['koordinate'] = koordinate.group('koordinate').replace('&nbsp;',' ')
+        seznam_podatkov.append(slovar)
+
+    # zapišimo to v csv in json datoteko:
+    orodja.zapisi_csv(seznam_podatkov, seznam_podatkov[0].keys(), os.path.join(mapa_gorovij[i], csv_gorovij[i]))
+    orodja.zapisi_json(seznam_podatkov, os.path.join(mapa_gorovij[i], json_gorovij[i]))
